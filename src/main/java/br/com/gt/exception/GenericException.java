5f3dc01ffdc4c1346eea325edcab2a80209fdf8d
Guilherme Tessaro <guilherme.tessaro27@gmail.com>
2021-09-12 20:22:10 -0300

    tratamento de erro generico

---
+++ src/main/java/br/com/gt/exception/GenericException.java
@@ -0,0 +1,12 @@
+package br.com.gt.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class GenericException extends RuntimeException {
+
+    public GenericException(String mensagem) {
+        super(mensagem);
+    }
+}
