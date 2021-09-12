8365f86442de361e97f2968ae66c56eae6bf0bf2
Guilherme Tessaro <guilherme.tessaro27@gmail.com>
2021-09-12 15:43:59 -0300

    Commit Inicial

---
+++ src/main/java/br/com/gt/GerneciadorDePautaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerneciadorDePautaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerneciadorDePautaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/gt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.gt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GerneciadorDePautaApplication.class);
+	}
+
+}
