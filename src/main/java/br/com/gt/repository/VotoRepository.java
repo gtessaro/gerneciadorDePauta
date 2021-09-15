deda1c5cc146ad8c70ff66e9d41744f223f312ea
Guilherme Tessaro <guilherme.tessaro27@gmail.com>
2021-09-14 21:36:44 -0300

    Ajuste para permitir apenas 1 voto por associado em cada pauta

--- src/main/java/br/com/gt/repository/VotoRepository.java
+++ src/main/java/br/com/gt/repository/VotoRepository.java
@@ -9,5 +9,6 @@ import br.com.gt.model.enumerator.VotoSessao;
 @Repository
 public interface VotoRepository extends JpaRepository<Voto, Integer>{
 	public Voto findByIdPautaAndIdAssociado(Integer idPauta, Integer idAssociado);
+	public Boolean existsByIdPautaAndIdAssociado(Integer idPauta, Integer idAssociado);
 	public Integer countByIdPautaAndVotoSessao(Integer idPauta, VotoSessao votoSessao);
 }
--- src/main/java/br/com/gt/service/VotoService.java
+++ src/main/java/br/com/gt/service/VotoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.gt.exception.GenericException;
 import br.com.gt.exception.NotFoundException;
+import br.com.gt.exception.UnauthorizedException;
 import br.com.gt.model.Associado;
 import br.com.gt.model.Voto;
 import br.com.gt.model.enumerator.VotoSessao;
@@ -22,18 +23,19 @@ public class VotoService {
 	private AssociadoService associadoService;
 	
 	public void realizarVoto(Integer idPauta, String cpf, String opcaoVoto){
+		
 		if(!pautaService.permiteVotar(idPauta)) {
 			throw new GenericException("Erro ao realizar voto.");
 		}
 		
 		Associado associado = associadoService.getAssociadoByCpf(cpf);
-		
-		Voto voto = (Voto) repository.findByIdPautaAndIdAssociado(idPauta, associado.getIdAssociado());
-		if(voto == null) {
-			voto = new Voto();
-			voto.setIdAssociado(associado.getIdAssociado());
-			voto.setIdPauta(idPauta);
+		if(!repository.existsByIdPautaAndIdAssociado(idPauta, associado.getIdAssociado())) {
+			throw new UnauthorizedException("O associado com cpf "+cpf+" já votou nesta pauta.");
 		}
+		
+		Voto voto = new Voto();
+		voto.setIdAssociado(associado.getIdAssociado());
+		voto.setIdPauta(idPauta);
 		voto.setVotoSessao(VotoSessao.valueOf(opcaoVoto));
 		repository.save(voto);
 	}
